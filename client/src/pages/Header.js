d9e6665a9405794d52042c1a012b14abe3c55332
Dipesh Jaiswal <dipeshjaiswal140@gmail.com>
2021-03-25 16:47:39 +0530

    setSearch as the value changes

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -1,40 +1,39 @@
-import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
 import SearchIcon from "@material-ui/icons/Search";
 import { Avatar } from "@material-ui/core";
 
-
+// Styles
 const useStyles = makeStyles({
-    header : {
-        display : "flex",
-        justifyContent: "space-between",
-        marginBottom: "30px",
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginBottom: "30px",
+  },
+
+  header__left: {
+    flex: 0.5,
+    minWidth: "70px",
+    backgroundColor: "white",
+    color: "gray",
+    borderRadius: "30px",
+    padding: "10px",
+    display: "flex",
+    alignItems: "center",
+
+    "& input": {
+      border: "none",
+      width: "100%",
+      outlineWidth: 0,
+      height: "100%",
     },
-
-    header__left : {
-        flex: 0.5,
-        minWidth: "70px",
-        backgroundColor: "white",
-        color: "gray",
-        borderRadius: "30px",
-        padding: "10px",
-        display: "flex",
-        alignItems: "center",
-
-        '& input': {
-            border: "none",
-            width: "100%",
-            outlineWidth: 0,      
-            height: "100%",
-        }
-    }
-})
-
+  },
+});
 
 function Header() {
-    const classes = useStyles()
-
+  const classes = useStyles();
+  const [search, setSearch] = useState("");
   return (
     <div className={classes.header}>
       <div className={classes.header__left}>
@@ -42,6 +41,8 @@ function Header() {
         <input
           type="text"
           placeholder="Search for Artists,Songs or Albums"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
       </div>
       <div className="header__right">
